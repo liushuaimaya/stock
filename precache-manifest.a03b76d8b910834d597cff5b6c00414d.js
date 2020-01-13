006c4dff18d1df3f37a75e2fa50028ae41e250b3
liushuai <313628216@qq.com>
2020-01-13 16:20:53 +0800

    Updates

---
+++ precache-manifest.a03b76d8b910834d597cff5b6c00414d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320ff583d9761a5e616f6be37b5dda17",
+    "url": "/stock/index.html"
+  },
+  {
+    "revision": "33b41403d7071b85f2a4",
+    "url": "/stock/static/css/main.1c36c410.chunk.css"
+  },
+  {
+    "revision": "0655d04fb95023fa153c",
+    "url": "/stock/static/js/2.6fa80938.chunk.js"
+  },
+  {
+    "revision": "33b41403d7071b85f2a4",
+    "url": "/stock/static/js/main.9cea7136.chunk.js"
+  },
+  {
+    "revision": "f817f2c2d01eabb2a5b4",
+    "url": "/stock/static/js/runtime-main.3897a8ee.js"
+  }
+]);

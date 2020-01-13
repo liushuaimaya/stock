5d8934b503d6cdad84db65e2a81a34deab49f19b
liushuai <313628216@qq.com>
2020-01-13 20:17:59 +0800

    Updates

--- precache-manifest.56b027ff8a023898a2addae3a9147230.js
+++ precache-manifest.56b027ff8a023898a2addae3a9147230.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "56899ddd2c97fad6b38776448ad22901",
+    "revision": "3f25c2b2085edcc337108a598e710011",
     "url": "/index.html"
   },
   {
-    "revision": "4ddec4dd927fb099c0f5",
-    "url": "/static/css/main.1c36c410.chunk.css"
+    "revision": "765514bbbfc5cd3c5a18",
+    "url": "/static/css/main.dcfe5404.chunk.css"
   },
   {
     "revision": "0655d04fb95023fa153c",
     "url": "/static/js/2.6fa80938.chunk.js"
   },
   {
-    "revision": "4ddec4dd927fb099c0f5",
+    "revision": "765514bbbfc5cd3c5a18",
     "url": "/static/js/main.cb124d51.chunk.js"
   },
   {
